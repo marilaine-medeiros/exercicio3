17b1ad39bbf35bdf73f31d7c53852e3ac346680c
Marilaine Medeiros <marilainemedeiros12@gmail.com>
2025-02-13 22:14:53 -0300

    exercicio3

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+        // para leitura do nome que será digitado
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do usuário: ");
+        String nome = scanner.nextLine();
+
+// loop para repetir o nome digitado 10 vezes
+        int quantidade = 1;
+        while (quantidade <= 10) {
+            System.out.println(quantidade + ": " + nome);
+            quantidade++;
+        }
+
+        scanner.close();
+    }
 }
